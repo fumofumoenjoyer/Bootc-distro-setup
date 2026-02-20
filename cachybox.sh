7921ad64ac58efdad43795387bfff4545003d668
Fumo <45699815+fumofumoenjoyer@users.noreply.github.com>
2026-02-20 02:57:15 -0300

    Update package installation and add new exports

--- cachybox.sh
+++ cachybox.sh
@@ -45,7 +45,7 @@ distrobox enter "${CONTAINER_NAME}" -- <<EOF
 echo "Installing packages..."
 sudo pacman -Sy --noconfirm archlinux-keyring
 sudo pacman -Syyu --noconfirm --needed base-devel git yay
-yay -Syu --noconfirm npm zsh bash spyder github-cli typescript eslint helm python-pip gdb gcc make libappindicator lib32-libappindicator maven jdk-openjdk kotlin clang llvm lld lldb libc++ cython libclc polly rustup antigravity gemini-cli google-chrome cachyos-gaming-applications cachyos-gaming-meta lsfg-vk-bin
+yay -Syu --noconfirm npm zsh bash spyder github-cli typescript eslint helm python-pip gdb gcc make libappindicator lib32-libappindicator bottles maven jdk-openjdk kotlin clang llvm lld lldb libc++ cython libclc polly rustup antigravity gemini-cli google-chrome cachyos-gaming-applications cachyos-gaming-meta lsfg-vk-bin
 rustup update
 rustup default stable
 rustup target add wasm32-unknown-unknown
@@ -58,6 +58,8 @@ distrobox-export --app antigravity
 distrobox-export --app steam
 distrobox-export --app lsfg-vk-ui
 distrobox-export --app goverlay
+distrobox-export --app heroic
+distrobox-export --app bottles
 
 EOF
 
